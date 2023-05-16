0a0024886f6796513d4a603562ddd4525ff31226
Ivan <kosarevsky2000@yandex.ru>
2023-05-16 22:11:31 +0300

    initial commit

---
+++ HomeWorkBasic-2-2/HomeWorkBasic-2-2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+struct bankAcount {
+    long long acountNumber{};
+    std::string name{};
+    double balance{};
+};
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    bankAcount info;
+
+    std::cout << "Введите номер счёта: ";
+    std::cin >> info.acountNumber;
+    std::cout << "Введите имя владельца: ";
+    std::cin >> info.name;
+    std::cout << "Введите баланс: ";
+    std::cin >> info.balance;
+    std::cout << "Введите новый баланс: ";
+    std::cin >> info.balance;
+    std::cout << "Ваш счёт: " << info.name << ", " << info.acountNumber << ", " << info.balance;
+    return 0;
+}
+
